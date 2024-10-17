17b3b7bcf49a947f5ef50c984e8bb3b08d81acf4
Darshita Patel <pateldarshita1994@gmail.com>
2024-10-17 07:05:05 -0400

    timer1

--- timer1.js
+++ timer1.js
@@ -3,15 +3,18 @@ const alarm = process.argv.splice(2)
 // check if user provided any input
 if (alarm.length === 0){
   console.log("please enter valid time");
+  return;
 } 
 
 for(let time of alarm){
   // check if user entrered valid input it should not be -ve number or string
   if (!Number(time)){
     console.log("you have entered string");
+    return;
   }
   if (Number(time) < 0){
-    console.log("You have entered -ve number");
+    console.log("You have entered -ve number");    
+    return;
   }
 
   setTimeout(() => {
